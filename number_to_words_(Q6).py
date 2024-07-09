e1982745ac40a871baf25508f255f4b80b137d02
AShiSH001000 <117503374+AShiSH001000@users.noreply.github.com>
2024-07-09 14:56:42 +0530

    Update number_to_words_(Q6).py
    output: 21124

--- number_to_words_(Q6).py
+++ number_to_words_(Q6).py
@@ -1,36 +1,34 @@
+def number_letter_counts():
+    units = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    teens = ["", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
+    tens = ["", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
+    hundred = "hundred"
+    thousand = "thousand"
+    
+    and_word = "and"
+    
+    def letter_count(n):
+        if n == 0:
+            return 0
+        if n < 10:
+            return len(units[n])
+        elif 10 < n < 20:
+            return len(teens[n-10])
+        elif n == 10 or n >= 20 and n < 100:
+            return len(tens[n // 10]) + len(units[n % 10])
+        elif n < 1000:
+            count = len(units[n // 100]) + len(hundred)
+            if n % 100 != 0:
+                count += len(and_word) + letter_count(n % 100)
+            return count
+        elif n == 1000:
+            return len(units[1]) + len(thousand)
+        return 0
 
-def number_to_words(n):
-    ones = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-    teens = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
-    tens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
-    thousands = ["", "thousand"]
+    total_count = 0
+    for i in range(1, 1001):
+        total_count += letter_count(i)
+    
+    return total_count
 
-    def get_words(num):
-        if num == 0:
-            return "zero"
-        words = []
-        if num >= 1000:
-            words.append(ones[num // 1000] + " thousand")
-            num %= 1000
-        if num >= 100:
-            words.append(ones[num // 100] + " hundred")
-            num %= 100
-        if num >= 20:
-            words.append(tens[num // 10])
-            num %= 10
-        elif num >= 10:
-            words.append(teens[num - 10])
-            num = 0
-        if num > 0:
-            words.append(ones[num])
-        return " ".join(words)
-
-    total_letters = 0
-    for i in range(1, n + 1):
-        words = get_words(i)
-        words = words.replace(" ", "").replace("-", "") 
-        total_letters += len(words)
-    return total_letters
-
-
-print(number_to_words(1000))
+number_letter_counts()
